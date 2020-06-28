234f03ba6eea943bf53e652fb97c97ae59418aa2
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2020-06-28 00:06:13 -0500

    add the starting files

---
+++ 02-ingress/ingress.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Never Directly Execute This Script
+return
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.NO_MASQ_LOCAL=1"
+
+# see what changes would be made, returns nonzero returncode if different
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+# Set Configuration
+kubectl apply -f config.yml
+
+#update ip configs
+kubectl delete po -n metallb-system --all
