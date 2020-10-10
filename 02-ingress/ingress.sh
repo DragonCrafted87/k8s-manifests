e474677edb6bc4d3aa212806550a40256de484ca
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2020-10-10 18:49:13 -0500

    current

--- 02-ingress/ingress.sh
+++ 02-ingress/ingress.sh
@@ -14,8 +14,8 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl apply -f namespace.yml
+kubectl apply -f metallb.yml
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
