de5d0fb1c76e894625287e7c3ce7a6b0eaa96b28
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2021-06-07 22:12:54 -0500

    current cluster state

--- _initial_setup/setup-cluster.sh
+++ _initial_setup/setup-cluster.sh
@@ -2,7 +2,7 @@ return
 
 sudo apt-get install -y nfs-common cifs-utils
 
-sudo snap install microk8s --classic --channel=1.19
+sudo snap install microk8s --classic --channel=1.20
 
 sudo iptables -P FORWARD ACCEPT
 sudo apt-get install iptables-persistent
@@ -10,6 +10,24 @@ sudo apt-get install iptables-persistent
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 
+# change k8s version
+sudo snap refresh microk8s --classic --channel=1.20
+
+# reset snap
+snap list
+sudo snap remove core18
+sudo snap remove snapd
+
+sudo apt autoremove --purge snapd gnome-software-plugin-snap
+
+rm -rf ~/snap
+sudo rm -rf /snap
+sudo rm -rf /var/snap
+sudo rm -rf /var/lib/snapd
+sudo umount /var/snap
+
+sudo mount -a
+sudo apt install snapd
 
 # first node
 microk8s status --wait-ready
@@ -23,5 +41,8 @@ microk8s add-node
 # other nodes
 microk8s join ***************************************************
 
+# get the .kube/config file
+microk8s config
 
+# apply all the manifests
 kubectl apply -f . --recursive
